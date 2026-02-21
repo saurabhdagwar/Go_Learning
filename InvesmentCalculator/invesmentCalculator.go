cc8728b4a8f1989e360a5497e97c0bca0074fdfd
saurabhdagwar <saurabhdagwar2020@gmail.com>
2026-02-21 12:58:37 +0530

    Fixed the code of Profit calculator

--- InvesmentCalculator/invesmentCalculator.go
+++ InvesmentCalculator/invesmentCalculator.go
@@ -1,13 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+const inflationRate = 2.5
 
 func main() {
-	fmt.Println("Investment Calculator")
-	var invesmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	invesmentAmount := outputText("Enter Invesment Value: ")
+	years := outputText("Enter Year: ")
+	expectedReturnRate := outputText("Enter Expected Return Rate:")
 
-	var futureValue = float64(invesmentAmount) * (1 + expectedReturnRate/100) * float64(years)
-	fmt.Printf("Future Value of the Investment: %.2f\n", futureValue)
+	futureValue, futureRealValue := calcFutureValue(invesmentAmount, expectedReturnRate, years)
+
+	formattedFV := fmt.Sprintf("Future Value: %.2f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future Real value: %.2f", futureRealValue)
+	fmt.Print(formattedFV, formattedRFV)
+}
+
+func outputText(text string) (num float64) {
+	fmt.Print(text)
+	fmt.Scan(&num)
+	return num
+}
+
+func calcFutureValue(invesmentAmount, expectedReturnRate, years float64) (fv float64, rfv float64) {
+	fv = invesmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv = fv / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
 }
